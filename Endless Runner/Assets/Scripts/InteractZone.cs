dd8c3d8e3931b51c55cfe73df132c469d1019fbe
dobisfreee <amber981001@gmail.com>
2024-09-20 19:45:42 +0900

    (2024.9.20) 안개 효과 시스템

--- Endless Runner/Assets/Scripts/InteractZone.cs
+++ Endless Runner/Assets/Scripts/InteractZone.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class InteractZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        IInteractable interactable = other.GetComponent <IInteractable>() ;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (interactable !=null)
+        {
+            interactable.Interact();
+        }
     }
 }
--- Endless Runner/Assets/Scripts/RoadManager.cs
+++ Endless Runner/Assets/Scripts/RoadManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class RoadManager : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
+    [SerializeField] float offset = 40.0f;
     [SerializeField] List<GameObject> roads;
-
-
-
+   
 
 
     void Start()
     {
         roads.Capacity = 10;
+
+
     }
 
     void Update()
@@ -23,5 +24,18 @@ public class RoadManager : MonoBehaviour
             roads[i].transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
         
+    }
+
+    public void InitializePosition()
+    {
+        GameObject newRoad = roads[0];
+
+        roads.Remove(newRoad); 
+
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+
+        newRoad.transform.position = new Vector3(0,0, newZ);
+
+        roads.Add(newRoad);
     }
 }
